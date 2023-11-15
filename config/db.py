5274bf8980758b518e50bb6b42086b5bca5ecce6
youssef KADI DEVOPS <youssef.kadi@devops.com>
2023-11-15 14:02:05 +0000

    add hpa db.py update with secret passwd and ingress

--- config/db.py
+++ config/db.py
@@ -1,6 +1,11 @@
 from sqlalchemy import create_engine, MetaData
+import base64
 
-engine = create_engine("postgresql://admin:password@db:5432/storedb")
+# Récupérer le mot de passe du Secret
+password_base64 = "UHJvamVjdERldm9wczIwMjMh"  # Remplacez par la valeur réelle depuis votre Secret
+password = base64.b64decode(password_base64).decode("utf-8")
+
+engine = create_engine(f"postgresql://admin:{password}@db:5432/storedb")
 
 meta = MetaData()
 
